0b54793c4c9803dfc4237837cadb5155ecfa3dfa
angelayc <angelaisuychen@gmail.com>
2025-03-26 17:48:55 +1100

    Added Subject class

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Subject.java
@@ -0,0 +1,11 @@
+package com.unimelb.swen30006.workshops;
+
+public class Subject {
+    private String name;
+    private String subjectCode;
+
+    public Subject(String name, String subjectCode) {
+        this.name = name;
+        this.subjectCode = subjectCode;
+    }
+}
